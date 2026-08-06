cf36570701f5fcbad53b28b4031f56c80f20abcf
estockda <69396603+estockda@users.noreply.github.com>
2026-08-06 16:12:35 -0400

    Refactor AWS authentication and add mock cassette reset method
    Restructures the WaterGuru API client to support the upcoming cassette replacement feature while keeping the codebase DRY.
    
    - Extracts the AWS Cognito and AWS4Auth credential generation out of the `get()` method into a reusable `_get_auth_and_user()` private method.
    - Adds a stubbed `reset_cassette(self, water_body_id: str)` method to allow the Home Assistant button entity and lockout logic to be tested locally without throwing an AttributeError.
    - Prepares the class for the payload capture, as the real reset endpoint will require the exact same AWS authentication tokens now provided by the helper method.

--- custom_components/waterguru/waterguru.py
+++ custom_components/waterguru/waterguru.py
@@ -23,12 +23,9 @@ class WaterGuru:
         self._username = username
         self._password = password
         self._session = session
-
-    def get(self):
-        """Get the latest data from the WaterGuru API."""
-
-        _LOGGER.info("Fetching data from WaterGuru API...")
-
+        
+    def _get_auth_and_user(self):
+        """Handle AWS Cognito authentication and return auth object and userId."""
         region_name = "us-west-2"
         pool_id = "us-west-2_icsnuWQWw"
         identity_pool_id = "us-west-2:691e3287-5776-40f2-a502-759de65a8f1c"
@@ -38,6 +35,7 @@ class WaterGuru:
         boto3.setup_default_session(region_name = region_name)
         client = boto3.client('cognito-idp', region_name=region_name)
         aws = AWSSRP(username=self._username, password=self._password, pool_id=pool_id, client_id=client_id, client=client)
+        
         try:
             tokens = aws.authenticate_user()
         except botocore.exceptions.ClientError as e:
@@ -46,8 +44,7 @@ class WaterGuru:
         id_token = tokens['AuthenticationResult']['IdToken']
         refresh_token = tokens['AuthenticationResult']['RefreshToken']
         access_token = tokens['AuthenticationResult']['AccessToken']
-        token_type = tokens['AuthenticationResult']['TokenType']
-
+        
         u=Cognito(pool_id,client_id,id_token=id_token,refresh_token=refresh_token,access_token=access_token)
         user = u.get_user()
         userId = user._metadata['username']
@@ -59,20 +56,28 @@ class WaterGuru:
 
         credentials_response = identity_client.get_credentials_for_identity(IdentityId=identity_id,Logins={idp_pool:id_token})
         credentials = credentials_response['Credentials']
-        access_key_id = credentials['AccessKeyId']
-        secret_key = credentials['SecretKey']
-        service = 'lambda'
-        session_token = credentials['SessionToken']
-        expiration = credentials['Expiration']
+        
+        auth = AWS4Auth(
+            credentials['AccessKeyId'], 
+            credentials['SecretKey'], 
+            region_name, 
+            'lambda', 
+            session_token=credentials['SessionToken']
+        )
+        
+        return auth, userId
+
+    def get(self):
+        """Get the latest data from the WaterGuru API."""
+        _LOGGER.info("Fetching data from WaterGuru API...")
+
+        auth, userId = self._get_auth_and_user()
 
         method = 'POST'
         headers = {'User-Agent': 'aws-sdk-iOS/2.24.3 iOS/14.7.1 en_US invoker', 'Content-Type': 'application/x-amz-json-1.0'}
         body = {"userId":userId, "clientType":"WEB_APP", "clientVersion":"0.2.3", "clip": False}
-        service = 'lambda'
         url = 'https://lambda.us-west-2.amazonaws.com/2015-03-31/functions/prod-getDashboardView/invocations'
-        region = 'us-west-2'
 
-        auth = AWS4Auth(access_key_id, secret_key, region, service, session_token=session_token)
         try:
             response = requests.request(method, url, auth=auth, json=body, headers=headers, timeout=9.9)
         except requests.exceptions.Timeout as e:
@@ -80,3 +85,18 @@ class WaterGuru:
 
         data = response.json()
         return {waterBodyData['waterBodyId']: WaterGuruDevice(waterBodyData) for waterBodyData in data['waterBodies']}
+
+    def reset_cassette(self, water_body_id: str):
+        """
+        MOCK: Reset the cassette life.
+        Actual endpoint and payload to be captured on next cassette replacement.
+        """
+        _LOGGER.info("MOCK: Resetting cassette for %s", water_body_id)
+        
+        # auth, userId = self._get_auth_and_user()
+        # method = 'POST'
+        # headers = {'User-Agent': 'aws-sdk-iOS/2.24.3 iOS/14.7.1 en_US invoker', 'Content-Type': 'application/x-amz-json-1.0'}
+        # body = {"userId": userId, "waterBodyId": water_body_id, ...}
+        # url = 'https://lambda.us-west-2.amazonaws.com/2015-03-31/functions/prod-replaceCassette/invocations'
+        
+        return True
